b28639122ce94bb6ffa263b1a1d5b3a562755884
chensonglin <893969679@qq.com>
2026-03-24 18:42:10 +0800

    补充：方法说明补充

--- frame_extractor.py
+++ frame_extractor.py
@@ -21,6 +21,7 @@ def extract_frames_fixed_interval(
         fps=1,
         output_dir=None
 ):
+    # fps每秒抽多少帧
     cap = cv2.VideoCapture(video_path)
 
     video_fps = cap.get(cv2.CAP_PROP_FPS)
